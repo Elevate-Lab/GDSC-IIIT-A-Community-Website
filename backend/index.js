9ffed830b37717baa10141b0a0ab694e627034c5
Sampada Kathar <Sampada101k@gmail.com>
2021-10-15 12:29:29 +0530

    dotenv added

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,28 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import  mongoose from 'mongoose';
+const express=require('express'),
+      bodyParser= require('body-parser');
+      mongoose= require('mongoose');
+
 
 const app = express();
+require('dotenv').config();
 
 app.use(bodyParser.json({limit: "30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 
 
+const url = process.env.MONGODB_URI || 3000;
 
-const CONNECTION_URL = "mongodb+srv://GDSCWeb:gdscop123@cluster0.mxecq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5000;
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}, () => {
+    console.log("Connected to database.");
+});
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>app.listen(PORT,()=>console.log(`Listening on ${PORT}`)))
-.catch((error)=>console.log(error.message));
+const port = process.env.PORT || 5000;
+
+app.listen(port, process.env.IP, () => {
+    console.log("showing on port 3000.");
+});
+
