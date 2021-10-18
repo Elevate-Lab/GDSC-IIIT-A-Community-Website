6c3ed385e19d6b2f4a899c27c267490d0fd3cf1a
Manav1410 <manavagarwal1410@gmail.com>
2021-10-18 19:00:21 +0530

    About Section update

--- frontend/src/pages/About/index.js
+++ frontend/src/pages/About/index.js
@@ -1,23 +1,27 @@
 import React from 'react'
 // import {Typography} from '@material-ui/core'
+import About_Image from './Assets/About_DSC_Image.png'
 
 function About() {
     return (
         <section style={{
-            height: '110.35vh',
+            height: '125.55vh',
             width:'100%',
             display: 'flex',
             flexDirection: 'column',
+            overflow: 'hidden',
+            gap:'10vh',
             // justifyContent: 'center',
             // alignItems: 'center',
             background: '#ffffff'
         }}>
         <div style={{
-            marginLeft:'9.02vw',
-            marginTop:'9.57vh',
+            marginTop:'10.88vh',
             display: 'flex',
             flexDirection: 'column',
-            gap:'2px'
+            gap:'2px',
+            paddingLeft:'9.02%',
+            width: 'max-content'
         }}>
             <p style={{
                 fontWeight: 'bold',
@@ -25,7 +29,8 @@ function About() {
                 lineHeight: '4.79vw',                
                 color: '#1B2733',
                 margin: '0',
-                padding: '0'
+                padding: '0',
+                width: 'max-content'
             }}>About The Community</p>
             <p style={{
                 fontWeight: '600',
@@ -33,10 +38,34 @@ function About() {
                 lineHeight: '1.53vw',                
                 color: '#637282',
                 margin: '0',
-                padding: '0'
+                padding: '0',
+                width: 'max-content'
             }}>What we do and How we are so good at it... !</p>
         </div>
-        
+        <div style={{
+            display: 'flex',
+            gap: '6.11vw',
+            width: 'max-content',
+            paddingLeft:'9.02%'
+            // flexDirection: 'row'
+        }}>
+            <img 
+                src={About_Image}
+                alt="About_DSC.jpg"
+                style={{
+                height: '20.83vw',
+                width:'37.99vw',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'fit',
+                boxShadow: '2px 2px 8px rgba(0, 0, 0, 0.25)',
+                borderRadius: '4px'
+            }}/>
+            <div style={{
+                height: '20.83vw',
+                width:'37.99vw',
+                background: 'red'
+            }}></div>
+        </div>
         </section>
     )
 }
