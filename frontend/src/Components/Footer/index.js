c3ca457d066c2537d83f91c4ee821db4a41d8c6d
Priyal Saxena <priyalsaxena66@gmail.com>
2021-10-23 00:06:30 +0530

    created nav and footer

--- frontend/src/Components/Footer/index.js
+++ frontend/src/Components/Footer/index.js
@@ -1,20 +1,33 @@
-import { Typography } from '@material-ui/core'
-import React from 'react'
+import { Typography } from "@material-ui/core";
+import React from "react";
+import "./styles.css";
+import { MdPlace, MdMail } from "react-icons/md";
 
 function Footer() {
-    return (
-        <div style={{
-            height: '11vh',
-            width: '100%',
-            backgroundColor: '#292C2F',
-            display: "flex",
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-        }}>
-            <Typography>Footer</Typography>
-        </div>
-    )
+	return (
+		<div className="footer">
+			<div className="mainContainer">
+				<div>
+					<h1>Google Developer Student Club</h1>
+					<p>Indian Institute of Information Technology Allahabad, Prayagraj</p>
+				</div>
+				<div>
+					<div className="row">
+						<MdMail className="icon" /> <div>xyz@gmail.com</div>
+					</div>
+					<div className="row">
+						<MdPlace className="icon" /> <div>Prayagraj</div>
+					</div>
+				</div>
+				<div>
+					<p>Follow us on:</p>
+				</div>
+			</div>
+			<hr />
+			<br />
+			<div className="copyright">© 2021 GDSC IIITA</div>
+		</div>
+	);
 }
 
-export default Footer
+export default Footer;
--- frontend/src/Components/Navbar/index.js
+++ frontend/src/Components/Navbar/index.js
@@ -1,41 +1,86 @@
-import React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import { ReactComponent as Logo } from "../../gdscLogo.svg";
+import "./styles.css";
+import DrawerComponent from "../Drawer/index";
+import useViewport from "../../viewport/useViewport";
+const navbarContent = ["About", "Events", "Team", "Projects", "Blogs"];
 
-const navbarContent = [
-    "About", "Events", "Team", "Projects", "Blogs", "Contact"
-]
+const Navbar = () => {
+	const { width, height, isMobile, isTablet } = useViewport();
+	return (
+		<div>
+			{false ? (
+				<DrawerComponent />
+			) : (
+				<AppBar
+					style={{
+						backgroundColor: "#fff",
+						height: "20vh",
+						justifyContent: "center",
+					}}
+					elevation={3}
+					position="static"
+				>
+					<Toolbar>
+						<Logo height="3.5em" style={{ paddingLeft: "9%" }} />
+						<Typography
+							style={{
+								color: "#000",
+								flexGrow: 2,
+								fontStyle: "bold",
+							}}
+						>
+							<div className="logoText">
+								<p className="text top">Developer Student Club</p>
+								<p className="text bottom">IIIT Allahabad</p>
+							</div>
+						</Typography>
+						<div
+							style={{
+								display: "flex",
+								flexDirection: "row",
+							}}
+						>
+							{navbarContent.map((item, index) => (
+								<Link style={{ textDecoration: "none" }} key={index} to={`${item}`}>
+									<Typography
+										style={{
+											color: "#4E4E4E",
+											marginLeft: 10,
+											marginRight: 10,
+											padding: "5px",
+											fontWeight: 600,
+										}}
+									>
+										{item}
+									</Typography>
+								</Link>
+							))}
 
-function Navbar() {
-    return (
-        <div>
-            <AppBar style={{
-                backgroundColor: '#ffffff',
-                boxShadow: 'none'
-            }} position="static">
-                <Toolbar>
-                    <Typography style={{
-                        color: '#000',
-                        flexGrow: 1
-                    }}>Navbar</Typography>
-                    <div style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                    }}>
-                        {
-                            navbarContent.map((item, index) => (
-                                <Link key={index} to={`${item}`}><Typography style={{
-                                    color: '#000',
-                                    marginLeft: 10,
-                                    marginRight: 10
-                                }}>{item}</Typography></Link>
-                            ))
-                        }
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
-}
+							<Button
+								href="/Contact"
+								variant="contained"
+								style={{
+									background: "#156EFF",
+									color: "#fff",
+									margin: "0px 10px",
+									padding: "5px 18px",
+									borderRadius: "38px",
+									fontWeight: 600,
+									// boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+								}}
+							>
+								Contact
+							</Button>
+						</div>
+					</Toolbar>
+				</AppBar>
+			)}
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
