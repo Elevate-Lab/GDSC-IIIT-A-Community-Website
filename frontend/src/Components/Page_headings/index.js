3fa233d30d8dfa0d3ce02a55d2b46dc7eb74d614
Priyal Saxena <priyalsaxena66@gmail.com>
2021-10-25 18:08:21 +0530

    resolved conflict

--- frontend/src/Components/Page_headings/index.js
+++ frontend/src/Components/Page_headings/index.js
@@ -1,16 +1,12 @@
-import React from 'react'
-import './Headings.css'
+import React from "react";
+import "./Headings.css";
 
-function Headings({LargeHeading,SmallHeading}) {
-    return (
-        <div className="Section_Headings">
-            <p  className="large_heading" >
-                {LargeHeading}
-            </p>
-            <p  className="small_heading">
-                {SmallHeading}
-            </p>
-        </div>
-
-        )}
-export default Headings
+function Headings({ LargeHeading, SmallHeading }) {
+	return (
+		<div className="Section_Headings">
+			<p className="large_heading">{LargeHeading}</p>
+			<p className="small_heading">{SmallHeading}</p>
+		</div>
+	);
+}
+export default Headings;
