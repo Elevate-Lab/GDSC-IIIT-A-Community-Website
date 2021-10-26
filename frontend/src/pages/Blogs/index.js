4364462e55003b24357337cb26204dcd6f92fe14
sauhard22 <sauhardsrivastava08@gmail.com>
2021-10-26 13:48:28 +0530

    grid made

--- frontend/src/pages/Blogs/index.js
+++ frontend/src/pages/Blogs/index.js
@@ -1,22 +1,121 @@
 import React from 'react'
 import DSBlogCard from '../../Components/DSBlogCard'
+import { Grid } from '@material-ui/core'
 
 function Blogs() {
     return (
         <div style={{
-            height: '100vh',
+            minHeight: '100vh',
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center'
         }}>
-            <DSBlogCard 
-            title="Learn Microinteraction" 
-            body="Lorem ipsum dolor sit amet, 
+            <Grid style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }} container>
+                <Grid style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }} item>
+                    <DSBlogCard
+                        title="Learn Microinteraction"
+                        body="Lorem ipsum dolor sit amet, 
             consectetur adipiscing elit. Ornare 
             pretium placerat ut platea. Purus 
             blandit integer sagittis massa vel est hac."
-            date="Monday Jan 20 , 2020" />
+                        date="Monday Jan 20 , 2020" />
+                </Grid>
+                <Grid style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }} item>
+                    <DSBlogCard
+                        title="Learn Microinteraction"
+                        body="Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Ornare 
+            pretium placerat ut platea. Purus 
+            blandit integer sagittis massa vel est hac."
+                        date="Monday Jan 20 , 2020" />
+                </Grid>
+                <Grid style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }} item>
+                    <DSBlogCard
+                        title="Learn Microinteraction"
+                        body="Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Ornare 
+            pretium placerat ut platea. Purus 
+            blandit integer sagittis massa vel est hac."
+                        date="Monday Jan 20 , 2020" />
+                </Grid>
+                <Grid style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }} item>
+                    <DSBlogCard
+                        title="Learn Microinteraction"
+                        body="Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Ornare 
+            pretium placerat ut platea. Purus 
+            blandit integer sagittis massa vel est hac."
+                        date="Monday Jan 20 , 2020" />
+                </Grid>
+                <Grid style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }} item>
+                    <DSBlogCard
+                        title="Learn Microinteraction"
+                        body="Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Ornare 
+            pretium placerat ut platea. Purus 
+            blandit integer sagittis massa vel est hac."
+                        date="Monday Jan 20 , 2020" />
+                </Grid>
+                <Grid style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }} item>
+                    <DSBlogCard
+                        title="Learn Microinteraction"
+                        body="Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Ornare 
+            pretium placerat ut platea. Purus 
+            blandit integer sagittis massa vel est hac."
+                        date="Monday Jan 20 , 2020" />
+                </Grid>
+                <Grid style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }} item>
+                    <DSBlogCard
+                        title="Learn Microinteraction"
+                        body="Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Ornare 
+            pretium placerat ut platea. Purus 
+            blandit integer sagittis massa vel est hac."
+                        date="Monday Jan 20 , 2020" />
+                </Grid>
+            </Grid>
         </div>
     )
 }
