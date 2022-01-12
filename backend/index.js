57b062584f8dac484f8c5ac59fa90de5969f03c4
Sampada Kathar <58398802+AlacritousCreature@users.noreply.github.com>
2022-01-12 15:36:40 +0530

    contact section

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,10 @@ const express       =require('express'),
       Event        = require("./models/event"),
       Blog         = require('./models/blog'),
       Team          = require('./models/team'),
-      project       =require('./models/project')
-       cors          =require('cors')
+      project       =require('./models/project'),
+      nodemailer    =require('nodemailer'),
+      cors          =require('cors')
+
 const eventRoutes = require("./routes/events");
 const blogRoutes = require('./routes/blogs')
 const teamRoutes = require('./routes/teams');
@@ -42,6 +44,58 @@ app.use("/events",eventRoutes);
 app.use("/blogs",blogRoutes);
 app.use("/teams",teamRoutes);
 app.use("/projects",projectRoutes);
+
+/*
+For testing contact section paste the below lines in env file with correct email and its password ensure that 
+the email address provided has 'less secure apps ON in security section of google account'
+EMAIL=''
+PASS=''
+*/
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    post: 587,
+    security: false,
+    auth:{
+        user: process.env.EMAIL,
+        pass: process.env.PASS
+    }
+})
+
+app.post('/mail',(req,res,next)=>{
+    var message=req.body.message
+    var name=req.body.name
+    var senderEmail=req.body.email
+
+    var mailOptions={
+        from: name,
+        to: 'gdsc@iiita.ac.in',
+        subject: "Message from GDSC IIIT A Community Website's Contact Section",
+        html: `Name: ${name} ${senderEmail} <br> Message: ${message}`
+    }
+
+    transporter.sendMail(mailOptions,(err,data)=>{
+        if(err){
+            res.json({
+                status: err
+            })
+            console.log(err)
+        }else{
+            res.json({
+                status: "success"
+            })
+            console.log("email sent "+data.response)
+        }
+    })
+})
+
+transporter.verify(function(err,success){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("server is ready to send emails")
+    }
+
+});
 
 const port = process.env.PORT || 5000;
 
