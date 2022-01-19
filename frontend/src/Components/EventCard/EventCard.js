7cdd3cf9e586621fecdab375dc56887006ff8c0e
Shailesh Tiwari <iib2020001@iiita.ac.in>
2022-01-19 22:09:52 +0530

    Fixed hyperlink issues from blog and event cards

--- frontend/src/Components/EventCard/EventCard.js
+++ frontend/src/Components/EventCard/EventCard.js
@@ -1,117 +1,123 @@
-import * as React from "react";
-import { useState,useContext } from 'react';
+import * as React from 'react';
+import { useState, useContext } from 'react';
 import apiContext from '../../ContextApi/ApiContext';
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import { Button, CardActionArea } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import CardMedia from "@mui/material/CardMedia";
-import styles from "./EventCard.module.css";
-import Headings from "../Page_headings";
-import image1 from "../../Assets/Images/img1.png";
-import { Link } from "react-router-dom";
-import { ReactComponent as RSVPsvg } from "../../Assets/svg_link.svg";
-import { ReactComponent as Calendar } from "../../Assets/calendar.svg";
-import { ReactComponent as Clock } from "../../Assets/clock.svg";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import { Button, CardActionArea } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
+import styles from './EventCard.module.css';
+import Headings from '../Page_headings';
+import image1 from '../../Assets/Images/img1.png';
+import { Link } from 'react-router-dom';
+import { ReactComponent as RSVPsvg } from '../../Assets/svg_link.svg';
+import { ReactComponent as Calendar } from '../../Assets/calendar.svg';
+import { ReactComponent as Clock } from '../../Assets/clock.svg';
 // import Stack from "@mui/material/Stack";
 
 function EventCard(props) {
-	const {event} = props
-    const context =useContext(apiContext)
-    const {removeData,previousCardData,removeEvent} = context
-	const [admin, setAdmin] = useState(true);
-	const [upcoming, setUpcoming] = useState(props.upcoming);
-	return (
-		// <Box className="card" sx={{ minWidth: 275 }}>
-		<Card variant="outlined" styles={{ padding: "0" }} className={styles.eventCard}>
-			{/* <CardMedia component="img" height="140" image="../Assets/About_DSC_Image.png" alt="project image" /> */}
+    const { event } = props;
+    const context = useContext(apiContext);
+    const { removeData, previousCardData, removeEvent } = context;
+    const [admin, setAdmin] = useState(true);
+    const [upcoming, setUpcoming] = useState(props.upcoming);
+    return (
+        // <Box className="card" sx={{ minWidth: 275 }}>
+        <Card variant="outlined" styles={{ padding: '0' }} className={styles.eventCard}>
+            {/* <CardMedia component="img" height="140" image="../Assets/About_DSC_Image.png" alt="project image" /> */}
 
-			{/* <CardMedia
+            {/* <CardMedia
 					component="img"
 					height="140"
 					image="http://www.lacor.info/film/a_la_folie/img/galerie/large/a_la_folie_06.jpg"
 					alt="project image"
 					className="img"
 				/> */}
-			<div className={styles.imgContainer}>
-				<img className={styles.img} src={event.image} alt="img" />
-			</div>
-			<div className={styles.content}>
-				<CardContent>
-					<div className={styles.container}>
-						<div className={styles.name}>
-							<Typography sx={{ fontSize: 18, fontWeight: "bold" }} color="text.primary" gutterBottom>
-								{event.name}
-							</Typography>
-							<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-								<Calendar className={styles.svg} />
-								{event.startDate} - {event.endDate}
-							</Typography>
-							<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-								<Clock className={styles.svg} />
-								Time
-							</Typography>
-						</div>
-					</div>
-					{/* <Headings LargeHeading="Project Name" SmallHeading="tagline" /> */}
+            <div className={styles.imgContainer}>
+                <img className={styles.img} src={event.image} alt="img" />
+            </div>
+            <div className={styles.content}>
+                <CardContent>
+                    <div className={styles.container}>
+                        <div className={styles.name}>
+                            <Typography
+                                sx={{ fontSize: 18, fontWeight: 'bold' }}
+                                color="text.primary"
+                                gutterBottom
+                            >
+                                {event.name}
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                <Calendar className={styles.svg} />
+                                {event.startDate} - {event.endDate}
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                <Clock className={styles.svg} />
+                                Time
+                            </Typography>
+                        </div>
+                    </div>
+                    {/* <Headings LargeHeading="Project Name" SmallHeading="tagline" /> */}
 
-					<div className={styles.text}>
-						<Typography variant="body2" style={{width:'90%'}}>
-						{event.description}
-						</Typography>
-					</div>
-				</CardContent>
+                    <div className={styles.text}>
+                        <Typography variant="body2" style={{ width: '90%' }}>
+                            {event.description}
+                        </Typography>
+                    </div>
+                </CardContent>
 
-				{upcoming && (
-					<CardActions className={styles.buttons}>
-						<Link to="" style={{ textDecoration: "none" }}>
-							<Button
-								className={styles.RSVPbtn}
-								size="small"
-								variant="contained"
-								disableElevation
-								style={{ backgroundColor: "#08ad5d" }}
-							>
-								<RSVPsvg className={styles.RSVPsvg} />
-								RSVP Here
-							</Button>
-						</Link>
-					</CardActions>
-				)}
+                {upcoming && (
+                    <CardActions className={styles.buttons}>
+                        <Button
+                            className={styles.RSVPbtn}
+                            size="small"
+                            variant="contained"
+                            disableElevation
+                            style={{ backgroundColor: '#08ad5d' }}
+                            href=""
+                        >
+                            <RSVPsvg className={styles.RSVPsvg} />
+                            RSVP Here
+                        </Button>
+                    </CardActions>
+                )}
 
-				{admin && (
-					<CardActions className={styles.buttonsAdmin}>
-						
-							<Button
-							 onClick={()=>removeEvent(event._id)}
-								className={styles.btn}
-								size="small"
-								variant="contained"
-								disableElevation
-								style={{ backgroundColor: "#EA4335" }}
-							>
-								Delete
-							</Button>
-						
-						<Link onClick={()=>previousCardData(event)} to="/Events/editEvent" style={{ textDecoration: "none" }}>
-							<Button
-								className={styles.btn}
-								size="small"
-								variant="outlined"
-								disableElevation
-								style={{ color: "#08ad5d", borderColor: "#08ad5d" }}
-							>
-								Edit
-							</Button>
-						</Link>
-					</CardActions>
-				)}
-			</div>
-		</Card>
-		// </Box>
-	);
+                {admin && (
+                    <CardActions className={styles.buttonsAdmin}>
+                        <Button
+                            onClick={() => removeEvent(event._id)}
+                            className={styles.btn}
+                            size="small"
+                            variant="contained"
+                            disableElevation
+                            style={{ backgroundColor: '#EA4335' }}
+                        >
+                            Delete
+                        </Button>
+
+                        <Link
+                            onClick={() => previousCardData(event)}
+                            to="/Events/editEvent"
+                            style={{ textDecoration: 'none' }}
+                        >
+                            <Button
+                                className={styles.btn}
+                                size="small"
+                                variant="outlined"
+                                disableElevation
+                                style={{ color: '#08ad5d', borderColor: '#08ad5d' }}
+                            >
+                                Edit
+                            </Button>
+                        </Link>
+                    </CardActions>
+                )}
+            </div>
+        </Card>
+        // </Box>
+    );
 }
 
 export default EventCard;
--- frontend/src/pages/Blogs/BlogCard.js
+++ frontend/src/pages/Blogs/BlogCard.js
@@ -1,122 +1,123 @@
-
-import { useState } from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Avatar from "@mui/material/Avatar";
-import styles from "./BlogCard.module.css";
-import { ReactComponent as ViewProject } from "../../Assets/svg_link.svg";
-import { ReactComponent as Github } from "../../Assets/Vector.svg";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import styles from './BlogCard.module.css';
+import { ReactComponent as ViewProject } from '../../Assets/svg_link.svg';
+import { ReactComponent as Github } from '../../Assets/Vector.svg';
+import { Link } from 'react-router-dom';
 // import Stack from "@mui/material/Stack";
-import image1 from "../../Assets/Images/img1.png";
-import { flexbox } from "@mui/system";
-import imag from "./thankyou.jpg"
-import {Images} from "./BlogData.js"
-import React, { useContext } from 'react'
-import apiContext from '../../ContextApi/ApiContext'
+import image1 from '../../Assets/Images/img1.png';
+import { flexbox } from '@mui/system';
+import imag from './thankyou.jpg';
+import { Images } from './BlogData.js';
+import React, { useContext } from 'react';
+import apiContext from '../../ContextApi/ApiContext';
 function BlogCard(props) {
-	const [admin, setAdmin] = useState(true);
-	const context = useContext(apiContext)
-    const {removeData,previousCardData,removeBlog} = context
-    const {blogs} = props
-	return (
-		<>
-		
-		<div>
-			<div style={{ marginBottom: '-2.6rem', zIndex: "0" }} >
-				<img src={blogs.image} style={{ width: '25rem', height: '17rem',cursor:'not-allowed' }} />
-			</div>
-			<div style={{ zIndex: "1" }}>
-				<Box className={styles.card} sx={{ minWidth: 275 }}>
-					<Card variant="outlined">
-						<CardContent>
-							<div style={{color:'black',fontWeight:'bold',textAlign:'center'}}>
-								<br></br>
-								{blogs.title}
-								
-							</div>
-							<br></br>
-							<div className={styles.text}>
-								<Typography variant="body2" sx={{ margin: 0 }}>
-									{blogs.description}
-								</Typography>
-							</div>
+    const [admin, setAdmin] = useState(true);
+    const context = useContext(apiContext);
+    const { removeData, previousCardData, removeBlog } = context;
+    const { blogs } = props;
+    return (
+        <>
+            <div>
+                <div style={{ marginBottom: '-2.6rem', zIndex: '0' }}>
+                    <img
+                        src={blogs.image}
+                        style={{ width: '25rem', height: '17rem', cursor: 'not-allowed' }}
+                    />
+                </div>
+                <div style={{ zIndex: '1' }}>
+                    <Box className={styles.card} sx={{ minWidth: 275 }}>
+                        <Card variant="outlined">
+                            <CardContent>
+                                <div style={{ color: 'black', fontWeight: 'bold', textAlign: 'center' }}>
+                                    <br></br>
+                                    {blogs.title}
+                                </div>
+                                <br></br>
+                                <div className={styles.text}>
+                                    <Typography variant="body2" sx={{ margin: 0 }}>
+                                        {blogs.description}
+                                    </Typography>
+                                </div>
 
-							<div className={styles.container} style={{ marginTop: '2rem' }}>
-								<div className={styles.img} style={{ marginBottom: '-0.5rem' }}>
-									<Avatar src={image1} sx={{ width: 28, height: 28 }} />
-								</div>
-								<div className={styles.name}>
-									<Typography
-										sx={{ fontSize: 14, margin: "0" }}
-										color="text.primary"
-										gutterBottom
-									>
-										{blogs.author}
-									</Typography>
-									<Typography sx={{ fontSize: 10, margin: "0" }} color="text.secondary" gutterBottom>
-										{blogs.date}
-									</Typography>
-								</div>
-							</div>
-						</CardContent>
+                                <div className={styles.container} style={{ marginTop: '2rem' }}>
+                                    <div className={styles.img} style={{ marginBottom: '-0.5rem' }}>
+                                        <Avatar src={image1} sx={{ width: 28, height: 28 }} />
+                                    </div>
+                                    <div className={styles.name}>
+                                        <Typography
+                                            sx={{ fontSize: 14, margin: '0' }}
+                                            color="text.primary"
+                                            gutterBottom
+                                        >
+                                            {blogs.author}
+                                        </Typography>
+                                        <Typography
+                                            sx={{ fontSize: 10, margin: '0' }}
+                                            color="text.secondary"
+                                            gutterBottom
+                                        >
+                                            {blogs.date}
+                                        </Typography>
+                                    </div>
+                                </div>
+                            </CardContent>
 
+                            <CardActions className={styles.buttons}>
+                                <Button
+                                    className={styles.btnP}
+                                    width="20rem"
+                                    variant="contained"
+                                    disableElevation
+                                    style={{ backgroundColor: '#08ad5d' }}
+                                    href={blogs.bloglink}
+                                >
+                                    <ViewProject className={styles.svg} />
+                                    Read on Medium
+                                </Button>
+                            </CardActions>
+                            {admin && (
+                                <CardActions className={styles.buttonsAdmin}>
+                                    <Button
+                                        onClick={() => removeBlog(blogs._id)}
+                                        className={styles.btn}
+                                        size="small"
+                                        variant="contained"
+                                        disableElevation
+                                        style={{ backgroundColor: '#EA4335' }}
+                                    >
+                                        Delete
+                                    </Button>
 
-
-						<CardActions className={styles.buttons}>
-
-							<Link to={blogs.bloglink} style={{ textDecoration: "none" }}>
-								<Button
-									className={styles.btnP}
-									width='20rem'
-
-									variant="contained"
-									disableElevation
-									style={{ backgroundColor: "#08ad5d" }}
-								>
-									<ViewProject className={styles.svg} />
-									Read on Medium
-								</Button>
-							</Link>
-						</CardActions>
-						{admin && (
-							<CardActions className={styles.buttonsAdmin}>
-								
-									<Button
-									onClick={()=>removeBlog(blogs._id)}
-										className={styles.btn}
-										size="small"
-										variant="contained"
-										disableElevation
-										style={{ backgroundColor: "#EA4335" }}
-									>
-										Delete
-									</Button>
-							
-								<Link to="/Blogs/EditBlog" onClick={()=>previousCardData(blogs)} style={{ textDecoration: "none" }}>
-									<Button
-									
-										className={styles.btn}
-										size="small"
-										variant="outlined"
-										disableElevation
-										style={{ color: "#08ad5d", borderColor: "#08ad5d" }}
-									>
-										Edit
-									</Button>
-								</Link>
-							</CardActions>
-						)}
-					</Card>
-				</Box>
-			</div>
-		</div>
-		</>
-	);
+                                    <Link
+                                        to="/Blogs/EditBlog"
+                                        onClick={() => previousCardData(blogs)}
+                                        style={{ textDecoration: 'none' }}
+                                    >
+                                        <Button
+                                            className={styles.btn}
+                                            size="small"
+                                            variant="outlined"
+                                            disableElevation
+                                            style={{ color: '#08ad5d', borderColor: '#08ad5d' }}
+                                        >
+                                            Edit
+                                        </Button>
+                                    </Link>
+                                </CardActions>
+                            )}
+                        </Card>
+                    </Box>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default BlogCard;
