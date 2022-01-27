0a8a56e6076a751c77c5717e3281da00def2a6c0
Shinigami <razam305@gmail.com>
2022-01-27 11:44:25 +0530

    Core Team and project updated

--- frontend/src/pages/Team/Teams_field/CoreTeam.js
+++ frontend/src/pages/Team/Teams_field/CoreTeam.js
@@ -6,13 +6,24 @@ import apiContext from '../../../ContextApi/ApiContext'
 
 function CoreTeam() {
     const context = useContext(apiContext)
-    const { coreTeam,data, getAllData, getAttribute,getAllTeamData,teams,parameter} = context
-    let attribute = "teams"
+    const { data, getAllData, getAttribute,getAllTeamData,teams,parameter} = context
+   // let attribute = "teams"
+    const [count , setCount] = useState(teams);
+  
     useEffect(() => {
         getAttribute(null)
         // getAllData();
         getAllTeamData();
-    }, [parameter])
+        // let newCount = count -1;
+        // setCount(newCount)
+        // if(count <= 0){
+        //     setCount(null)
+        // }
+        // console.log(count);
+    }, [ parameter ])
+    const coreTeam = teams && teams.filter((e) => {
+        return e.designation === 'Core Team';
+    })
     return (
             <div id="CoreTeam" className="Field_view">
             {coreTeam &&
