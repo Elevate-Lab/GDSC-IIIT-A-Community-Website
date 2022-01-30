1942dc8916802cb4244369be4d2acb9a7de403f5
Shailesh Tiwari <iib2020001@iiita.ac.in>
2022-01-30 23:53:00 +0530

    Added Animated arrow in hero section

--- frontend/src/pages/Home/Hero.js
+++ frontend/src/pages/Home/Hero.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import './Hero.css';
-import Herologo from '../../Assets/DSC-IIITA-Light-Vertical.png'
-import ExploreIcon from '../../Assets/chevrons-down.svg'
+import Herologo from '../../Assets/DSC-IIITA-Light-Vertical.png';
+import ExploreIcon from '../../Assets/chevrons-down.svg';
 
 import HeroLeft from './HeroLeft';
 import HeroRight from './HeroRight';
 
+const move = () => {
+    window.scrollTo({
+        top: window.innerHeight,
+        behavior: 'smooth',
+    });
+};
+
 function Hero() {
-    
     return (
         <section className="hero-section">
-            <HeroLeft/>
+            <HeroLeft />
             <div className="hero-section-center">
                 <div className="hero-section-main">
-                    <img src={Herologo} alt="Hero Logo"/>
+                    <img src={Herologo} alt="Hero Logo" />
                     <div className="hero-join-us">
-                        <a href="https://gdsc.community.dev/indian-institute-of-information-technology-allahabad-prayagraj/" target="_blank" rel="noopener noreferrer">
+                        <a
+                            href="https://gdsc.community.dev/indian-institute-of-information-technology-allahabad-prayagraj/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
                             Join Us
                         </a>
                     </div>
                 </div>
                 <div class="hero-explore">
-                    <p>Explore</p>
-                    <img src={ExploreIcon} alt="Explore"/>
+                    {/* <p>Explore</p>
+                    <img src={ExploreIcon} alt="Explore"/> */}
+                    <img
+                        onClick={move}
+                        src={ExploreIcon}
+                        alt="arrow"
+                        color="white"
+                        className="animatedArrow"
+                    />
                 </div>
             </div>
-            <HeroRight/>
+            <HeroRight />
         </section>
     );
 }
