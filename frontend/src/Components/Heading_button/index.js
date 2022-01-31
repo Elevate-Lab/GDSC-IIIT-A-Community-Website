9616e5801d4049f987709eb73b19cf980eb50870
Shailesh Tiwari <iib2020001@iiita.ac.in>
2022-01-31 19:38:00 +0530

    fixed button

--- frontend/src/Components/Heading_button/index.js
+++ frontend/src/Components/Heading_button/index.js
@@ -1,28 +1,29 @@
-import { getThemeProps } from "@material-ui/styles";
-import React from "react";
-import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
-import "./Headings_button.css";
+import { getThemeProps } from '@material-ui/styles';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import './Headings_button.css';
 
 function HeadingButton({ LargeHeading, SmallHeading, bg }) {
-	return (
-		<div className="Headings_Button_Container">
-			<div className="Section_Headings">
-				<p className="large_heading">{LargeHeading}</p>
-				<p className="small_heading">{SmallHeading}</p>
-			</div>
+    return (
+        <div className="Headings_Button_Container">
+            <div className="Section_Headings">
+                <p className="large_heading">{LargeHeading}</p>
+                <p className="small_heading">{SmallHeading}</p>
+            </div>
 
-			{/* <div className="Button">
+            {/* <div className="Button">
 				<Link to="/contact" style={{ textDecoration: "none" }}>
 					{ButtonText}
 				</Link>
 			</div> */}
-			<div className="Button" style={{ background: "#EFF" }}>
-				<Link to="/contact" style={{ textDecoration: "none" }}>
-					<img src={bg} alt="Youtube" />
-				</Link>
-			</div>
-		</div>
-	);
+            <div className="Button" style={{ background: '#EFF' }}>
+                <Link to="/contact" style={{ textDecoration: 'none' }}>
+                    {/* help */}
+                    <img src={bg} alt="Youtube" />
+                </Link>
+            </div>
+        </div>
+    );
 }
 export default HeadingButton;
